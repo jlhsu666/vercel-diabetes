c5102dc80f129147ffcde5d39c1afae6ad90b47d
Your Name <you@example.com>
2023-03-25 15:27:45 +0800

    revised

--- api/index.py
+++ api/index.py
@@ -27,31 +27,31 @@ def aml():
     "GlobalParameters": {}
     }
 
-body = str.encode(json.dumps(data))
+    body = str.encode(json.dumps(data))
 
-url = 'http://20.70.9.109:80/api/v1/service/brain/score'
-# Replace this with the primary/secondary key or AMLToken for the endpoint
-api_key = '9KOw0kBlhyVauSeGfgeu72LH4UEx52Wd'
+    url = 'http://20.70.9.109:80/api/v1/service/brain/score'
+    # Replace this with the primary/secondary key or AMLToken for the endpoint
+    api_key = '9KOw0kBlhyVauSeGfgeu72LH4UEx52Wd'
 
-if not api_key:
-    raise Exception("A key should be provided to invoke the endpoint")
+    if not api_key:
+        raise Exception("A key should be provided to invoke the endpoint")
 
 
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-req = urllib.request.Request(url, body, headers)
+    req = urllib.request.Request(url, body, headers)
 
-try:
-    response = urllib.request.urlopen(req)
+    try:
+        response = urllib.request.urlopen(req)
 
-    result = response.read()
-    print(result)
-except urllib.error.HTTPError as error:
-    print("The request failed with status code: " + str(error.code))
+        result = response.read()
+        print(result)
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
 
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
-    print(error.read().decode("utf8", 'ignore'))
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(error.read().decode("utf8", 'ignore'))
 
 
 @app.route('/about')
